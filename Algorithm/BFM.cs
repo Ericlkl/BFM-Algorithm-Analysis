e7c0d9d7b92ada9452ca4221364299738c951d81
LEE KA LONG (Eric) <33568780+Ericlkl@users.noreply.github.com>
2019-05-01 22:55:25 +1000

    Basic Operation problem fix

--- Algorithm/BFM.cs
+++ Algorithm/BFM.cs
@@ -69,6 +69,7 @@ namespace Algorithm
                     {
                         numsmaller++;
                     }
+                    basicOperation++;
                     else if (array[j] == array[i]) // Basic Operation, Comparision
                     {
                         numequal++; 
